6f571ca672eae9eea1bf0902918c5f4a2bc2ab89
Hanisah Fildza Annafisah <93438625+fildzaanf@users.noreply.github.com>
2023-12-24 02:13:46 +0700

    feature(bcrypt): add hash and compare password

--- utils/helper/bcrypt/bcrypt.go
+++ utils/helper/bcrypt/bcrypt.go
@@ -1,2 +1,18 @@
 package bcrypt
 
+import (
+	"golang.org/x/crypto/bcrypt"
+)
+
+func HashPassword(password string) string {
+	bytes, _ := bcrypt.GenerateFromPassword([]byte(password), bcrypt.DefaultCost)
+	return string(bytes)
+}
+
+func ComparePassword(hash, password string) error {
+	err := bcrypt.CompareHashAndPassword([]byte(hash), []byte(password))
+	if err != nil {
+		return err
+	}
+	return nil
+}
