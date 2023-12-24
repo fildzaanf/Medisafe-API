3325d03c13951b226ab8057fa76965b0ab66c870
Hanisah Fildza Annafisah <93438625+fildzaanf@users.noreply.github.com>
2023-12-24 22:48:00 +0700

    feat(response): add success, error, pagination response

--- utils/responses/pagination.go
+++ utils/responses/pagination.go
@@ -1,1 +1,20 @@
 package responses
+
+type TSuccessResponsePage struct {
+	Meta    TResponseMetaPage `json:"meta"`
+	Results interface{}       `json:"results"`
+}
+
+func SuccessResponsePage(message string, page int, limit int, total int64, data interface{}) interface{} {
+	return TSuccessResponsePage{
+		Meta: TResponseMetaPage{
+			Success: true,
+			Message: message,
+			Page:    page,
+			Limit:   limit,
+			Total:   total,
+		},
+		Results: data,
+	}
+}
+
