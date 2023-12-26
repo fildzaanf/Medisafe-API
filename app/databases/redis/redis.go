c92d57137352376cd980035e3237ef2895626525
Hanisah Fildza Annafisah <93438625+fildzaanf@users.noreply.github.com>
2023-12-26 19:34:53 +0700

    feat(databases): add redis

--- app/databases/redis/redis.go
+++ app/databases/redis/redis.go
@@ -1,2 +1,33 @@
 package redis
 
+import (
+	"fmt"
+	"talkspace/app/configs"
+
+	"github.com/go-redis/redis"
+	"github.com/sirupsen/logrus"
+)
+
+func ConnectRedis() *redis.Client {
+	config, err := configs.LoadConfig()
+	if err != nil {
+		logrus.Fatalf("failed to load redis configuration: %v", err)
+		return nil
+	}
+
+	client := redis.NewClient(&redis.Options{
+		Addr: fmt.Sprintf("%s:%s",
+		 	  		config.REDIS.REDIS_HOST,
+		      		config.REDIS.REDIS_PORT),
+		DB:   1,
+	})
+
+	_, err = client.Ping().Result()
+	if err != nil {
+		logrus.Errorf("failed to connect to redis: %v", err)
+		return nil
+	}
+
+	logrus.Info("connected to redis")
+	return client
+}
