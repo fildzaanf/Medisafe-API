15be4bc9ee845788fa6cb1d12777e9ba1851f74a
Hanisah Fildza Annafisah <93438625+fildzaanf@users.noreply.github.com>
2023-12-29 22:06:27 +0700

    feat(user): add models and hooks

--- features/user/model/model.go
+++ features/user/model/model.go
@@ -1,1 +1,26 @@
 package model
+
+import (
+	"time"
+
+	"gorm.io/gorm"
+)
+
+type User struct {
+	ID             string `gorm:"primarykey"`
+	Fullname       string
+	Email          string `gorm:"not null"`
+	Password       string `gorm:"not null"`
+	ProfilePicture string
+	Birthdate      string
+	Gender         string `gorm:"type:enum('male', 'female');default:null"`
+	BloodType      string `gorm:"type:enum('A', 'B', 'O', 'AB');default:null"`
+	Height         int
+	Weight         int
+	Role           string `gorm:"type:enum('user');default:'user'"`
+	OTP            string `gorm:"not null"`
+	IsVerified     bool   `gorm:"not null;default:false"`
+	CreatedAt      time.Time
+	UpdatedAt      time.Time
+	DeletedAt      gorm.DeletedAt `gorm:"index"`
+}
