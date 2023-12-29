6bb7152c23823ee51a0ae3705fc8fe82d9c4f0ee
Hanisah Fildza Annafisah <93438625+fildzaanf@users.noreply.github.com>
2023-12-29 22:04:54 +0700

    feat(user): add dto response and mapping

--- features/user/dto/response/mapping.go
+++ features/user/dto/response/mapping.go
@@ -1,1 +1,26 @@
 package response
+
+type UserRegisterResponse struct {
+	ID       string `json:"id"`
+	Fullname string `json:"fullname"`
+	Email    string `json:"email"`
+}
+
+type UserLoginResponse struct {
+	ID       string `json:"id"`
+	Fullname string `json:"fullname"`
+	Email    string `json:"email"`
+	Token    string `json:"token"`
+}
+
+type UserProfileResponse struct {
+	ID             string `json:"id"`
+	Fullname       string `json:"fullname"`
+	Email          string `json:"email"`
+	ProfilePicture string `json:"profile_picture"`
+	Gender         string `json:"gender"`
+	Birthdate      string `json:"birthdate"`
+	BloodType      string `json:"blood_type"`
+	Height         int    `json:"height"`
+	Weight         int    `json:"weight"`
+}
