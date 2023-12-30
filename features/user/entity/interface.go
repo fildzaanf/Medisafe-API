0ea7b0d3d6184379214f0a91cc711f35527e36ac
Hanisah Fildza Annafisah <93438625+fildzaanf@users.noreply.github.com>
2023-12-31 05:27:35 +0700

    feat(user): update user (entity,interface)

--- features/user/entity/interface.go
+++ features/user/entity/interface.go
@@ -6,6 +6,8 @@ type UserRepositoryInterface interface {
 	Register(userCore User) (User, error)
 	GetByID(id string) (User, error)
 	UpdateByID(id string, userCore User) error
+	UpdatePassword(id string, userCore User) error
+	NewPassword(email string, userCore User) (User, error)
 	FindByEmail(email string) (User, error)
 	GetByVerificationToken(token string) (User, error)
 	UpdateIsVerified(id string, isVerified bool) error
@@ -19,6 +21,8 @@ type UserServiceInterface interface {
 	Login(email, password string) (User, string, error)
 	GetByID(id string) (User, error)
 	UpdateByID(id string, userCore User) error
+	UpdatePassword(id string, userCore User) error 
+	NewPassword(email string, userCore User) error
 	VerifyUser(token string) (bool, error)
 	UpdateIsVerified(id string, isVerified bool) error
 	SendOTP(email string) error
@@ -30,6 +34,9 @@ type UserHandlerInterface interface {
 	Login(c echo.Context) error
 	GetUserByID(c echo.Context) error
 	UpdateByID(c echo.Context) error
+	UpdatePassword(c echo.Context) error
+	ForgotPassword(c echo.Context) error 
+	NewPassword(c echo.Context) error
 	VerifyAccount(c echo.Context) error
 	VerifyOTP(c echo.Context) error
 }
