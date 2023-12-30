63396e752677ea4592e32e2b2e2e1afbfac5ef12
Hanisah Fildza Annafisah <93438625+fildzaanf@users.noreply.github.com>
2023-12-31 06:29:38 +0700

    feat(user): update user handler

--- features/user/handler/handler.go
+++ features/user/handler/handler.go
@@ -2,13 +2,16 @@ package handler
 
 import (
 	"net/http"
+	"strings"
 	"talkspace/features/user/dto/request"
 	"talkspace/features/user/dto/response"
 	"talkspace/features/user/entity"
+	"talkspace/middlewares"
 	"talkspace/utils/constant"
+	"talkspace/utils/helper/generator"
 	"talkspace/utils/responses"
 
-	"github.com/labstack/echo"
+	"github.com/labstack/echo/v4"
 )
 
 type userHandler struct {
@@ -38,7 +41,7 @@ func (uh *userHandler) Register(c echo.Context) error {
 
 	response := response.UserCoreToUserRegisterResponse(request)
 
-	return c.JSON(http.StatusCreated, responses.SuccessResponse(constant.SUCCESS_CREATED, response))
+	return c.JSON(http.StatusCreated, responses.SuccessResponse(constant.SUCCESS_REGISTER, response))
 }
 
 func (uh *userHandler) Login(c echo.Context) error {
@@ -57,4 +60,112 @@ func (uh *userHandler) Login(c echo.Context) error {
 	response := response.UserCoreToUserLoginResponse(request, token)
 
 	return c.JSON(http.StatusOK, responses.SuccessResponse(constant.SUCCESS_LOGIN, response))
+}
+
+func (uh *userHandler) UpdatePassword(c echo.Context) error {
+	requestUpdatePasword := request.UserUpdatePasswordRequest{}
+
+	errBind := c.Bind(&requestUpdatePasword)
+	if errBind != nil {
+		return c.JSON(http.StatusBadRequest, responses.ErrorResponse(errBind.Error()))
+	}
+
+	userID, _, errExtractToken := middlewares.ExtractToken(c)
+	if errExtractToken != nil {
+		return c.JSON(http.StatusUnauthorized, responses.ErrorResponse(errExtractToken.Error()))
+	}
+
+	request := request.UserUpdatePasswordRequestToUserCore(requestUpdatePasword)
+
+	errUpdate := uh.userService.UpdatePassword(userID, request)
+	if errUpdate != nil {
+		return c.JSON(http.StatusBadRequest, responses.ErrorResponse(errUpdate.Error()))
+	}
+
+	return c.JSON(http.StatusOK, responses.SuccessResponse(constant.SUCCESS_PASSWORD_UPDATED, nil))
+}
+
+func (uh *userHandler) VerifyAccount(c echo.Context) error {
+	token := c.QueryParam("token")
+
+	userVerified, errVerified := uh.userService.VerifyUser(token)
+	if errVerified != nil {
+		return c.JSON(http.StatusBadRequest, responses.ErrorResponse(errVerified.Error()))
+	}
+
+	if userVerified {
+		email, errGenerateTemplate := generator.GenerateEmailTemplate("verification-account.html", nil)
+		if errGenerateTemplate != nil {
+			return c.JSON(http.StatusInternalServerError, responses.ErrorResponse(constant.ERROR_TEMPLATE_READER))
+		}
+		return c.HTML(http.StatusOK, email)
+	}
+
+	email, errGenerateTemplate := generator.GenerateEmailTemplate("verification-account-success.html", nil)
+	if errGenerateTemplate != nil {
+		return c.JSON(http.StatusInternalServerError, responses.ErrorResponse(constant.ERROR_TEMPLATE_READER))
+	}
+	return c.HTML(http.StatusOK, email)
+}
+
+func (uh *userHandler) ForgotPassword(c echo.Context) error {
+	requestOTP := request.UserSendOTPRequest{}
+
+	errBind := c.Bind(&requestOTP)
+	if errBind != nil {
+		return c.JSON(http.StatusBadRequest, responses.ErrorResponse(errBind.Error()))
+	}
+
+	request := request.UserSendOTPRequestToUserCore(requestOTP)
+
+	errSendOTP := uh.userService.SendOTP(request.Email)
+	if errSendOTP != nil {
+		if strings.Contains(errSendOTP.Error(), constant.ERROR_EMAIL_NOTFOUND) {
+			return c.JSON(http.StatusNotFound, responses.ErrorResponse(errSendOTP.Error()))
+		}
+		return c.JSON(http.StatusBadRequest, responses.ErrorResponse(errSendOTP.Error()))
+	}
+
+	return c.JSON(http.StatusOK, responses.SuccessResponse(constant.SUCCESS_OTP_SENT, nil))
+}
+
+func (uh *userHandler) VerifyOTP(c echo.Context) error {
+	requestVerifyOTP := request.UserVerifyOTPRequest{}
+
+	errBind := c.Bind(&requestVerifyOTP)
+	if errBind != nil {
+		return c.JSON(http.StatusBadRequest, responses.ErrorResponse(errBind.Error()))
+	}
+
+	request := request.UserVerifyOTPRequestToUserCore(requestVerifyOTP)
+
+	token, errVerify := uh.userService.VerifyOTP(request.Email, request.OTP)
+	if errVerify != nil {
+		return c.JSON(http.StatusInternalServerError, responses.ErrorResponse(constant.ERROR_OTP_VERIFY+errVerify.Error()))
+	}
+
+	return c.JSON(http.StatusOK, responses.SuccessResponse(constant.SUCCESS_OTP_VERIFIED, token))
+}
+
+func (uh *userHandler) NewPassword(c echo.Context) error {
+	requestNewPassword := request.UserNewPasswordRequest{}
+
+	errBind := c.Bind(&requestNewPassword)
+	if errBind != nil {
+		return c.JSON(http.StatusBadRequest, responses.ErrorResponse(errBind.Error()))
+	}
+
+	email, errExtract := middlewares.ExtractVerifyToken(c)
+	if errExtract != nil {
+		return c.JSON(http.StatusUnauthorized, responses.ErrorResponse(errExtract.Error()))
+	}
+
+	request := request.UserNewPasswordRequestToUserCore(requestNewPassword)
+
+	err := uh.userService.NewPassword(email, request)
+	if err != nil {
+		return c.JSON(http.StatusBadRequest, responses.ErrorResponse(err.Error()))
+	}
+
+	return c.JSON(http.StatusOK, responses.SuccessResponse(constant.SUCCESS_PASSWORD_UPDATED, nil))
 }
