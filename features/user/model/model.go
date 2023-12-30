fc8b09d8c48ce5426ef4269bd8e29f0f47f441c4
Hanisah Fildza Annafisah <93438625+fildzaanf@users.noreply.github.com>
2023-12-31 05:29:26 +0700

    feat(user): update user model

--- features/user/model/model.go
+++ features/user/model/model.go
@@ -7,21 +7,22 @@ import (
 )
 
 type User struct {
-	ID             string `gorm:"primarykey"`
-	Fullname       string
-	Email          string `gorm:"not null"`
-	Password       string `gorm:"not null"`
-	ProfilePicture string
-	Birthdate      string
-	Gender         string `gorm:"type:enum('male', 'female');default:null"`
-	BloodType      string `gorm:"type:enum('A', 'B', 'O', 'AB');default:null"`
-	Height         int
-	Weight         int
-	Role           string `gorm:"type:enum('user');default:'user'"`
-	OTP            string `gorm:"not null"`
-	OTPExpiration  int64
-	IsVerified     bool `gorm:"not null;default:false"`
-	CreatedAt      time.Time
-	UpdatedAt      time.Time
-	DeletedAt      gorm.DeletedAt `gorm:"index"`
+	ID                string `gorm:"primarykey"`
+	Fullname          string
+	Email             string `gorm:"not null"`
+	Password          string `gorm:"not null"`
+	ProfilePicture    string
+	Birthdate         string
+	Gender            string `gorm:"type:enum('male', 'female');default:null"`
+	BloodType         string `gorm:"type:enum('A', 'B', 'O', 'AB');default:null"`
+	Height            int
+	Weight            int
+	Role              string `gorm:"type:enum('user');default:'user'"`
+	OTP               string `gorm:"not null"`
+	OTPExpiration     int64
+	VerificationToken string
+	IsVerified        bool `gorm:"not null;default:false"`
+	CreatedAt         time.Time
+	UpdatedAt         time.Time
+	DeletedAt         gorm.DeletedAt `gorm:"index"`
 }
