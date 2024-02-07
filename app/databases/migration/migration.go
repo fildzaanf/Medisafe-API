433409e3ed018e75fcc6a7b186f3b043b6b092f4
Hanisah Fildza Annafisah <hanisahfildza@gmail.com>
2024-02-07 23:56:39 +0700

    update migration databases

--- app/databases/migration/migration.go
+++ app/databases/migration/migration.go
@@ -1,9 +1,19 @@
 package migration
 
 import (
-    "gorm.io/gorm"
+	consultation "talkspace/features/consultation/model"
+	doctor "talkspace/features/doctor/model"
+	transaction "talkspace/features/transaction/model"
+	user "talkspace/features/user/model"
+
+	"gorm.io/gorm"
 )
 
 func Migrate(db *gorm.DB) {
-    db.AutoMigrate()
+	db.AutoMigrate(
+		&user.User{},
+		&doctor.Doctor{},
+		&transaction.Transaction{},
+		&consultation.Consultation{},
+	)
 }
