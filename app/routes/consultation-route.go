33b7860d815bd418d3c025d733a4f2dd77542eb2
Hanisah Fildza Annafisah <hanisahfildza@gmail.com>
2024-02-10 00:50:51 +0700

    feat(routes): update routes

--- app/routes/consultation-route.go
+++ app/routes/consultation-route.go
@@ -1,9 +1,31 @@
 package routes
 
+import (
+	"talkspace/middlewares"
+
+	"github.com/go-redis/redis"
+	"github.com/labstack/echo/v4"
+	"gorm.io/gorm"
+)
+
 func ConsultationRoutes(e *echo.Group, db *gorm.DB, rdb *redis.Client) {
 
 	consultationRepository := repository.NewConsultationRepository(db, rdb)
 	consultationService := service.NewConsultationService(consultationRepository)
 	consultationHandler := handler.NewConsultationHandler(consultationService)
 
+	doctor := e.Group("doctor")
+	doctor.GET("", consultationHandler.GetAllDoctors)
+	doctor.GET("/:doctor_id", consultationHandler.GetDoctorByID)
+
+	roomchat := e.Group("/roomchat", middlewares.JWTMiddleware())
+	roomchat.POST("/:transaction_id", consultationHandler.CreateRoomchat)
+	roomchat.GET("", consultationHandler.GetAllRoomchats)
+	roomchat.GET("/:roomchat_id", consultationHandler.GetRoomchatByID)
+
+	message := e.Group("/message", middlewares.JWTMiddleware())
+	message.POST("/:roomchat_id", consultationHandler.CreateMessage)
+
 }
+
+
--- app/routes/doctor-route.go
+++ app/routes/doctor-route.go
@@ -18,18 +18,22 @@ func DoctorRoutes(e *echo.Group, db *gorm.DB, rdb *redis.Client) {
 	doctorHandler := handler.NewDoctorHandler(doctorService)
 
 	account := e.Group("/account")
-	account.POST("register", doctorHandler.Register)
-	account.GET("verify-account", doctorHandler.VerifyAccount)
-	account.POST("login", doctorHandler.Login)
+	account.POST("register", doctorHandler.RegisterDoctorAccount)
+	account.GET("verify-account", doctorHandler.VerifyDoctorAccount)
+	account.POST("login", doctorHandler.LoginDoctorAccount)
 
 	password := e.Group("/password")
 	password.POST("forgot-password", doctorHandler.ForgotPassword)
 	password.POST("verify-otp", doctorHandler.VerifyOTP)
 	password.PATCH("new-password", doctorHandler.NewPassword, middlewares.JWTMiddleware())
+	password.PATCH("/change-password", doctorHandler.UpdatePassword, middlewares.JWTMiddleware())
 
 	profile := e.Group("/profile", middlewares.JWTMiddleware())
-	profile.GET("", doctorHandler.GetDoctorByID)
-	profile.PUT("", doctorHandler.UpdateByID)
-	profile.PATCH("/change-password", doctorHandler.UpdatePassword)
+	profile.GET("", doctorHandler.GetDoctorProfileByID)
+	profile.PUT("", doctorHandler.UpdateDoctorProfileByID)
+
+	
 
 }
+
+
